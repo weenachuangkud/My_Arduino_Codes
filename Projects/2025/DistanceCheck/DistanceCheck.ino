23e5dd06326f38e0da6cc99b60b43c81fa48b14b
Mawin Chuangkud <weena.chuangkud@gmail.com>
2026-02-11 21:47:16 +0700

    Create DistanceCheck.ino

---
+++ Projects/2025/DistanceCheck/DistanceCheck.ino
@@ -0,0 +1,24 @@
+/*
+    - Author: Mawin CK
+    - Date: 2025
+*/
+// Link to project:
+
+#include "HCSR04.h"
+
+const byte ECHO = 12;
+const byte TRIG = 13;
+
+
+UltraSonicDistanceSensor distanceSensor(TRIG, ECHO);
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  float distance = distanceSensor.measureDistanceCm();
+  Serial.println(distance);
+
+  delay(500);
+}
